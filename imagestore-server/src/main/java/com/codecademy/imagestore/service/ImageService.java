9b398bc6aecbba0c6965cd66d60f3240bc4de8b1
sanjays <sanjay.suresh0695@gmail.com>
2024-02-08 16:56:53 +0530

    removed unwanted imports

--- imagestore-server/src/main/java/com/codecademy/imagestore/service/ImageService.java
+++ imagestore-server/src/main/java/com/codecademy/imagestore/service/ImageService.java
@@ -21,9 +21,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Slf4j
